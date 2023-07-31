01f5967f5e86d986ee7fc6b3a49f0f605cb0cb16
Benjamin Diomat <benjamin.diomat@gmail.com>
2023-07-31 15:01:01 +0200

    commit input

--- Components/Input.js
+++ Components/Input.js
@@ -11,32 +11,6 @@ import {
   
   export default function Input(props) {
     return (
-<<<<<<< HEAD
-        <TextInput  style = {styles.input}
-                    placeholder = {props.placeholder}
-                    cursorColor={props.cursorColor}
-                    maxLength={props.maxLength}
-                    keyboardType={props.keyboardType}
-                    onChangeText={props.onChangeText}>
-        </TextInput>
-
-)};
-
-const styles = StyleSheet.create({
-input: {
-    width: '85%',
-    height: 60,
-    backgroundColor: '#F5F5F5',
-    marginBottom: 20,
-    borderRadius: 8,
-    padding: 20,
-    borderColor:'#afb1b6',
-    borderWidth:1,
-    opacity: 0.8,
-},
-});
-
-=======
       <View> 
       
       <TextInput
@@ -90,4 +64,3 @@ input: {
     
   });
   
->>>>>>> frontend
